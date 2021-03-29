a9a3ef0db6d1403271413369b2073b979385df56
water-a <aliu.developer@gmail.com>
2021-03-29 00:06:33 -0400

    fix(linting)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,17 @@
 module.exports = {
   extends: ['airbnb-typescript/base', 'prettier'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: 'tsconfig.json',
+      },
+    },
+  ],
   rules: {
     'import/prefer-default-export': 'off',
     'no-param-reassign': 'off',
+    'import/no-extraneous-dependencies': 'off',
   },
 };
