64f0879c3d94f1b20ef2fd70dd1e980027f78419
water-a <aliu.developer@gmail.com>
2021-03-28 23:46:43 -0400

    fix(eslint)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,5 @@
 module.exports = {
   extends: ['airbnb-typescript/base', 'prettier'],
-  parserOptions: {
-    project: './tsconfig.json',
-  },
   rules: {
     'import/prefer-default-export': 'off',
     'no-param-reassign': 'off',
